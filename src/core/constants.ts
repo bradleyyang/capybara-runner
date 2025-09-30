3db5cc37a69d77b0b9b9597c80e7ee9193763d5a
bradleyyang <bradley.yang023@gmail.com>
2025-09-30 01:23:57 -0400

    minor changes to improve controls

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,5 +1,5 @@
 export const GROUND_Y = 410;
-export const JUMP_STRENGTH = -15;
+export const JUMP_STRENGTH = -20;
 export const GRAVITY = 1.2;
-export const OBSTACLE_SPEED = 6;
+export const OBSTACLE_SPEED = 8;
 export const SPAWN_INTERVAL = 100;
